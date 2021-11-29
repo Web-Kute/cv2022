7e923cfc9f326db8e783e8553a269f1303b97d41
Ikigai <denis.godec@weekub.fr>
2021-11-29 16:01:43 +0100

    Add sprites images

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,20 @@
+'use strict';
+
+//Creation context and hoisting (hissage)
+console.log(a);
 var a = "Hello World";
 
 function greet() {
-    return "WTF"
+    return console.log("WTF");
 }
+greet();
+
+//ex: On appelle la fonction avant de la créer
+nomChat("Chloé");
+
+function nomChat(nom) {
+    console.log("Le nom de mon chat est " + nom);
+}
+
+// Pendant la phase de création la fonction et la variable sont mises en mémoire
+// Mais pas la valeur de la variable
